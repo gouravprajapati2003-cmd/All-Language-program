d195b3ff87a5630b4c5e92ffb01a993a0bbcec09
gouravprajapati2003-cmd <gouravprajapati8132@gmail.com>
2026-04-02 21:52:06 +0530

    Add files via upload

---
+++ p2/MyPackage.java
@@ -0,0 +1,13 @@
+// This is the Package program.
+package p2;
+import p1.*;
+
+public class MyPackage {
+    public void show() {
+        System.out.println("Hello User, You Are in P2 Package");
+    }
+    public static void main(String[] args) {
+       Test obj = new Test();
+      obj.show();
+    }
+}

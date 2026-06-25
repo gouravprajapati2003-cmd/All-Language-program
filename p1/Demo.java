39696c467c28a77f133f8adc6a904449c9e92670
gouravprajapati2003-cmd <gouravprajapati8132@gmail.com>
2026-06-25 23:10:34 +0530

    Remove unwanted files

--- p1/Demo.java
+++
@@ -1,9 +0,0 @@
-package p1;
-import p2.MyPackage;
-
-public class Demo {
-    public static void main(String[] args) {
-        MyPackage obj = new MyPackage();
-        obj.show();
-    }
-}
--- p2/LambdaThread.java
+++
@@ -1,8 +0,0 @@
-package p2;
-// Lambda Exoression Thread Program;
-public class LambdaThread {
-    public static void main(String[] args) {
-        Thread t1 = new Thread(()->{System.out.println("With The Help of Lambda Expression");});
-        t1.start();
-    }
-}
